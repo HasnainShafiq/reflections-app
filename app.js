61eaf4cddeae00df9505dddc2f5fbe14f6450f46
Uncreative-creativity <31780746+Uncreative-creativity@users.noreply.github.com>
2022-07-14 18:49:53 +0100

    Separated routes. Adding session & flash

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const path = require('path');
 /* const morgan = require('morgan'); */
 // require mongo model
 const Reflection = require('./models/reflection');
+const reflectionsRouter = require('./routes/reflections');
 var mongoose = require('mongoose');
 // used to create PUT and DELETE routes
 const methodOverride = require('method-override');
 // used to add layouts to ejs
 const engine = require('ejs-mate')
+const session = require('express-session');
 // config file for mongo atlas
 const config = require("./config");
 const dbUrl = config.dbUrl;
@@ -34,70 +36,26 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({extended: true})); 
 app.use(methodOverride('_method'));
 
+const sessionConfig = {
+  secret: 'thisismysecret',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+    httpOnly: true,
+  }
+}
+
+app.use(session(sessionConfig));
+
+// for any routes handled by the reflectionsRouter, give them all a preceding '/reflections'
+app.use('/reflections', reflectionsRouter);
 
 app.engine('ejs', engine);
 app.set('view engine', 'ejs');
 // set views path relative to this root file + '/views'
 app.set('views', path.join(__dirname, 'views'));
-
-
-// on home page, display all posts
-app.get('/reflection', async (req, res) => {
-  // when performing any database function, must use await.
-  const reflections = await Reflection.find({});
-                    // include reflections as a variable to be used in index.ejs
-  res.render('index', { reflections })
-})
-
-
-app.get('/reflection/new', async (req, res) => {
-  const reflection = Reflection;
-  res.render('new', { reflection });
-})
-
-// send a request to show a new post to the index page & save to db 
-app.post('/reflection', async (req, res) => {
-  // req.body.reflection = anything whose name is in format of: reflection[property], e.g. reflection[prompt], which === reflection.prompt
-  const reflection = new Reflection(req.body.reflection);
-  await reflection.save();
-  res.redirect(`/reflection/${reflection._id}`);
-})
-
-// get a specific post
-app.get('/reflection/:id', async (req, res) => {
-  const { id } = req.params;
-  const reflection = await Reflection.findById(id);
-  res.render('show', { reflection });
-})
-
-// get edit page for specific post
-app.get('/reflection/:id/edit', async (req, res) => {
-  const { id } = req.params;
-  const reflection = await Reflection.findById(id);
-  res.render('edit', { reflection })
-})
-
-
-// edit specific post
-app.put('/reflection/:id', async (req, res) => {
-  const { id } = req.params;
-  // update reflection with new changes made in any elements with name = 'reflection'.
-  const reflection = await Reflection.findByIdAndUpdate(id, {...req.body.reflection}, { runValidators: true, new: true});
-  console.log(req.body);
-  res.redirect(`/reflection/${reflection._id}`);
-})
-
-// delete specific post
-app.delete('/reflection/:id', async (req, res) => {
-  const { id } = req.params;
-  await Reflection.findByIdAndDelete(id);
-  res.redirect('/reflection');
-})
-
-// if none of the above routes are found, send this status code
-app.use((req, res) => {
-  res.status(404).send("NOT FOUND");
-})
 
 app.listen('3000', () => {
   console.log('listening on port 3000');
---
+++ routes/reflections.js
@@ -0,0 +1,65 @@
+const express = require('express');
+// allows us to create our own routes and export them in a module
+const router = express.Router();
+// require mongo model
+const Reflection = require('../models/reflection');
+
+// on home page, display all posts
+router.get('/', async (req, res) => {
+    // when performing any database function, must use await.
+    const reflections = await Reflection.find({});
+    // include reflections as a variable to be used in index.ejs
+    res.render('index', { reflections })
+})
+
+// no preceding '/reflections'
+router.get('/new', async (req, res) => {
+    const reflection = Reflection;
+    res.render('new', { reflection });
+})
+
+// send a request to show a new post to the index page & save to db 
+router.post('/', async (req, res) => {
+    // req.body.reflection = anything whose name is in format of: reflection[property], e.g. reflection[prompt], which === reflection.prompt
+    const reflection = new Reflection(req.body.reflection);
+    await reflection.save();
+    res.redirect(`/reflections/${reflection._id}`);
+})
+
+// get a specific post
+router.get('/:id', async (req, res) => {
+    const { id } = req.params;
+    const reflection = await Reflection.findById(id);
+    res.render('show', { reflection });
+})
+
+// get edit page for specific post
+router.get('/:id/edit', async (req, res) => {
+    const { id } = req.params;
+    const reflection = await Reflection.findById(id);
+    res.render('edit', { reflection })
+})
+
+
+// edit specific post
+router.put('/:id', async (req, res) => {
+    const { id } = req.params;
+    // update reflection with new changes made in any elements with name = 'reflection'.
+    const reflection = await Reflection.findByIdAndUpdate(id, { ...req.body.reflection }, { runValidators: true, new: true });
+    console.log(req.body);
+    res.redirect(`/reflections/${reflection._id}`);
+})
+
+// delete specific post
+router.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+    await Reflection.findByIdAndDelete(id);
+    res.redirect('/reflections');
+})
+
+// if none of the above routes are found, send this status code
+router.use((req, res) => {
+    res.status(404).send("NOT FOUND");
+})
+
+module.exports = router;
